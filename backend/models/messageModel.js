7ed932692c8cb264fc3f2b8d1cfc5be989cceba3
yashksaini-coder <ys3853428@gmail.com>
2024-10-17 21:16:48 +0530

    Add field for link previews

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -6,6 +6,12 @@ const messageSchema = mongoose.Schema(
     content: { type: String, trim: true },
     chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
     readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    linkPreviews: [{ // Add this field for link previews
+      title: { type: String },
+      description: { type: String },
+      url: { type: String },
+      image: { type: String },
+    }],
   },
   { timestamps: true }
 );
